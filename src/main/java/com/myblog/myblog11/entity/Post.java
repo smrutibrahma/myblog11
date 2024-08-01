9cf8e0d1d6a85e9a57af0e96820e33e3bc2a6a09
smrut <smrutibrahma1@gmail.com>
2024-08-01 23:35:05 +0530

    127:Create save Post Feature Entity class added 01 Aug 2024

--- src/main/java/com/myblog/myblog11/entity/Post.java
+++ src/main/java/com/myblog/myblog11/entity/Post.java
@@ -14,7 +14,6 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String title;
     private String description;
     private String content;
